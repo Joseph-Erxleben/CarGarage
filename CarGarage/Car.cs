d8619245b308e975f0cad184701ecee92980ace4
Joseph Erxleben <jerxleben.13@gmail.com>
2021-01-29 14:13:39 -0500

    First Commit, all tests pass.

--- CarGarage/Car.cs
+++ CarGarage/Car.cs
@@ -12,5 +12,78 @@ namespace CarGarage
         public int FuelLevel { get; set; }
         public bool AutoBrakeActive { get; set; }
 
+        public void SetMake(string make)
+        {
+            Make = make;
+        }
+
+        public void SetModel(string model)
+        {
+            Model = model;
+        }
+
+        public int GetSpeed()
+        {
+            return Speed;
+        }
+
+        public bool IsSpeeding()
+        {
+            bool isSpeeding;
+
+            if(Speed>70)
+            {
+                isSpeeding = true;
+            }
+
+            else
+            {
+                isSpeeding = false;
+            }
+
+            return isSpeeding;
+        }
+
+        public void Accelerate()
+        {
+            if (Make == null)
+            {
+                Make = "";
+            }
+
+            if (Make.Equals("Ferrari"))
+            {
+                Speed = Speed + 20;
+            }
+
+            else
+            {
+                Speed = Speed + 10;
+            }
+
+            if (Speed > 100)
+            {
+                Speed = 100;
+            }
+        }
+
+        public void Brake()
+        {
+            if (AutoBrakeActive == true)
+            {
+                Speed = Speed / 2;
+            }
+
+            else
+            {
+                Speed = Speed - 7;
+            }
+
+            if (Speed < 0)
+            {
+                Speed = 0;
+            }
+        }
     }
+
 }
